0737ae68ffea291f08015d6adb5ca30a2b6b2b67
Mirna Medeiros <mirna.medeiros.053@imd.ufrm.br>
2024-05-31 20:14:50 -0300

    Created the home page (topbar and footer)

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {faCircleUser} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLink,
+    FontAwesomeModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  faCircleUser = faCircleUser;
 }
